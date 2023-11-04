932d9b0b59f1152f217033c069f836fe043e1ac8
238758 <103256074+238758@users.noreply.github.com>
2023-11-04 11:32:11 +0100

    Update theApp.py

--- theApp.py
+++ theApp.py
@@ -2,8 +2,16 @@ import streamlit as st
 from datetime import datetime, timedelta
 
 def id_to_month_year(month_id, base_year=1990, base_month=1):
-    start_date = datetime(base_year, base_month, 1)
-    target_date = start_date + timedelta(month_id - 1)
+    # Calculate the target year and month based on the month ID
+    target_year = base_year + (month_id - 1) // 12
+    target_month = (base_month + (month_id - 1) % 12) % 12
+    if target_month == 0:
+        target_month = 12
+        target_year -= 1
+
+    # Construct the target date
+    target_date = datetime(target_year, target_month, 1)
+
     return target_date.strftime("%B %Y")
 
 st.title("ID to Month-Year Converter")
