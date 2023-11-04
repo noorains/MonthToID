c9c966df4509d7498b70920983816e0c47373d13
238758 <103256074+238758@users.noreply.github.com>
2023-11-04 14:13:32 +0100

    Update theApp.py

--- theApp.py
+++ theApp.py
@@ -2,25 +2,24 @@ import streamlit as st
 from datetime import datetime, timedelta
 
 def id_to_month_year(month_id, base_year=1980, base_month=1):
-    # Calculate the target year and month based on the month ID
     target_year = base_year + (month_id - 1) // 12
     target_month = (base_month + (month_id - 1) % 12) % 12
     if target_month == 0:
         target_month = 12
         target_year -= 1
 
-    # Construct the target date
     target_date = datetime(target_year, target_month, 1)
-
     return target_date.strftime("%B %Y")
 
-st.title("VIEWS ID to Month-Year Converter")
+st.title("ID to Month-Year Converter")
 
 month_id = st.number_input("Enter the month ID (1 for January 1980, 2 for February 1980, and so on):", min_value=1, value=1)
 
-if st.button("Convert"):
-    result = id_to_month_year(month_id)
-    #st.write(f"The corresponding month is: {result}")
-    st.markdown(f"<p style='font-size:24px;'><b>The corresponding month for id {month_id} is: {result}</b></p>", unsafe_allow_html=True)
+if isinstance(month_id, int):
+    if st.button("Convert"):
+        result = id_to_month_year(month_id)
+        st.markdown(f"<p style='font-size:24px;'><b>The corresponding month for month_id {month_id} is: {result}</b></p>", unsafe_allow_html=True)
+else:
+    st.error("Please enter a valid integer.")
 
 st.write("Note: 1 = January 1980, 2 = February 1980, and so on.")
