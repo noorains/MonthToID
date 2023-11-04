d93518dcc5b0a7f202fbf481f42f112e54790b1a
238758 <103256074+238758@users.noreply.github.com>
2023-11-04 11:36:45 +0100

    update

--- theApp.py
+++ theApp.py
@@ -20,6 +20,6 @@ month_id = st.number_input("Enter the month ID (1 for January 1980, 2 for Februa
 
 if st.button("Convert"):
     result = id_to_month_year(month_id)
-    st.write(f"The corresponding date is: {result}")
+    st.write(f"The corresponding month is: {result}")
 
 st.write("Note: 1 = January 1980, 2 = February 1980, and so on.")
